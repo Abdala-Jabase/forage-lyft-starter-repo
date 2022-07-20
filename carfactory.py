fd5a3ca2e23768a743e6b9f5884baca153685e7a
Abdala Jabase <abdala.jabase@gmail.com>
2022-07-20 15:57:33 +0200

    Make carfactory methods static

--- carfactory.py
+++ carfactory.py
@@ -8,30 +8,36 @@ from battery.spindlerBattery import SpindlerBattey
 from battery.nubbinBattery import NubbinBattery
 
 class CarFactory():
+
+    @staticmethod
     def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
         engine = CapuletEngine(last_service_mileage, current_mileage)
         battery = SpindlerBattey(last_service_date, current_date)
         car = Car(engine, battery)
         return car
-
+        
+    @staticmethod
     def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
         engine = WilloughbyEngine(last_service_mileage, current_mileage)
         battery = SpindlerBattey(last_service_date, current_date)
         car = Car(engine, battery)
         return car
 
+    @staticmethod
     def create_palindrome(current_date: date, last_service_date: date, warning_light_on: bool) -> Car:
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattey(last_service_date, current_date)
         car = Car(engine, battery)
         return car
 
+    @staticmethod
     def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
         engine = WilloughbyEngine(last_service_mileage, current_mileage)
         battery = NubbinBattery(last_service_date, current_date)
         car = Car(engine, battery)
         return car
 
+    @staticmethod
     def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
         engine = CapuletEngine(last_service_mileage, current_mileage)
         battery = NubbinBattery(last_service_date, current_date)
